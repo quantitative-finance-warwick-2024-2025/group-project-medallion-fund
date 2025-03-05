7210eceb554c6ff55651c49f72c994e125856e1f
Akshay_Z <akshayzine>
2025-03-05 14:44:09 +0000

    working main program with fileoutput and metric

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/akshayzine/Academics/Programming_Group_Project/Akshay_Branch/group-project-medallion-fund/tests
+# Build directory: /Users/akshayzine/Academics/Programming_Group_Project/Akshay_Branch/group-project-medallion-fund/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(my_test "/Users/akshayzine/Academics/Programming_Group_Project/Akshay_Branch/group-project-medallion-fund/build/tests/my_test")
+set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/akshayzine/Academics/Programming_Group_Project/Akshay_Branch/group-project-medallion-fund/tests/CMakeLists.txt;3;add_test;/Users/akshayzine/Academics/Programming_Group_Project/Akshay_Branch/group-project-medallion-fund/tests/CMakeLists.txt;0;")
